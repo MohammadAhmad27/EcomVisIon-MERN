cfd870982b5ac93095f9cebca9cb191e08990107
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2025-02-25 23:45:20 +0500

    Added redux state for light/dark theme and added <Navbar />

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
-function App() {
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from './theme.js'
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./scenes/dashboard/index.jsx"
+import Layout from "./scenes/layout/index.jsx"
+const App = () => {
 
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <>
-     <p>hekhvfvuhvuhvihfdhdfvhfdhviufdufdhvfdllo</p>
-    </>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   )
 }
 

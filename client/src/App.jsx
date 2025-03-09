2c9c976bf081d8023a48ee67cea2c2ab30004148
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2025-03-09 21:01:45 +0500

    Added <Daily /> and <Monthly />

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,8 @@ import Customers from "./scenes/customers/index.jsx";
 import Transactions from "./scenes/transactions/index.jsx";
 import Geography from "./scenes/geography/index.jsx";
 import Overview from "./scenes/overview/index.jsx";
+import Daily from "./scenes/daily/index.jsx";
+import Monthly from "./scenes/monthly/index.jsx";
 
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
@@ -29,6 +31,8 @@ const App = () => {
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
